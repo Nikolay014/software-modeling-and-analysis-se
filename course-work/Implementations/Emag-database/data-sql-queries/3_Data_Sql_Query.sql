3645dffaf6fec2a512b1c4411b3959d2e50dbe88
Nikolay014 <107757167+Nikolay014@users.noreply.github.com>
2025-11-15 16:53:24 +0200

    Add files via upload

---
+++ course-work/Implementations/Emag-database/data-sql-queries/3_Data_Sql_Query.sql
@@ -0,0 +1,159 @@
+
+-- Order (30 записа)
+INSERT INTO [ORDER] (ORDER_NUMBER, [USER_ID], ORDER_DATE, [STATUS], POSTAL_CODE, DELIVERY_ADDRESS, TOTAL_AMOUNT) VALUES
+('ORD-2025-0001', 1, '2025-02-01', 'Delivered', '1000', 'Sofia, ul. Vitosha 10', 0),
+('ORD-2025-0002', 2, '2025-02-02', 'Delivered', '4000', 'Plovdiv, ul. Trakia 5', 0),
+('ORD-2025-0003', 3, '2025-02-03', 'Delivered', '9000', 'Varna, ul. Cherno more 12', 0),
+('ORD-2025-0004', 4, '2025-02-04', 'Delivered', '8000', 'Burgas, ul. More 8', 0),
+('ORD-2025-0005', 5, '2025-02-05', 'Delivered', '7000', 'Ruse, ul. Dunav 3', 0),
+('ORD-2025-0006', 6, '2025-02-06', 'Delivered', '6000', 'Stara Zagora, ul. Beroe 22', 0),
+('ORD-2025-0007', 7, '2025-02-07', 'Delivered', '5800', 'Pleven, ul. Panorama 9', 0),
+('ORD-2025-0008', 8, '2025-02-08', 'Delivered', '1000', 'Sofia, bul. Bulgaria 15', 0),
+('ORD-2025-0009', 9, '2025-02-09', 'Delivered', '9000', 'Varna, ul. Sevastopol 1', 0),
+('ORD-2025-0010', 10, '2025-02-10', 'Delivered', '4000', 'Plovdiv, ul. Kamenitsa 7', 0),
+('ORD-2025-0011', 11, '2025-02-11', 'Shipped', '1000', 'Sofia, ul. Rakovska 21', 0),
+('ORD-2025-0012', 12, '2025-02-12', 'Shipped', '8000', 'Burgas, ul. Strandzha 11', 0),
+('ORD-2025-0013', 13, '2025-02-13', 'Shipped', '5800', 'Pleven, ul. Skobelev 4', 0),
+('ORD-2025-0014', 14, '2025-02-14', 'Shipped', '9000', 'Varna, ul. Vladislav 9', 0),
+('ORD-2025-0015', 15, '2025-02-15', 'Shipped', '1000', 'Sofia, ul. Dondukov 33', 0),
+('ORD-2025-0016', 1, '2025-02-16', 'Delivered', '1000', 'Sofia, ul. Vitosha 10', 0),
+('ORD-2025-0017', 2, '2025-02-17', 'Delivered', '4000', 'Plovdiv, ul. Trakia 5', 0),
+('ORD-2025-0018', 3, '2025-02-18', 'Delivered', '9000', 'Varna, ul. Cherno more 12', 0),
+('ORD-2025-0019', 4, '2025-02-19', 'Delivered', '8000', 'Burgas, ul. More 8', 0),
+('ORD-2025-0020', 5, '2025-02-20', 'Delivered', '7000', 'Ruse, ul. Dunav 3', 0),
+('ORD-2025-0021', 6, '2025-02-21', 'Pending',  '6000', 'Stara Zagora, ul. Beroe 22', 0),
+('ORD-2025-0022', 7, '2025-02-22', 'Pending',  '5800', 'Pleven, ul. Panorama 9', 0),
+('ORD-2025-0023', 8, '2025-02-23', 'Pending',  '1000', 'Sofia, bul. Bulgaria 15', 0),
+('ORD-2025-0024', 9, '2025-02-24', 'Pending',  '9000', 'Varna, ul. Sevastopol 1', 0),
+('ORD-2025-0025', 10, '2025-02-25', 'Pending', '4000', 'Plovdiv, ul. Kamenitsa 7', 0),
+('ORD-2025-0026', 11, '2025-02-26', 'Delivered', '1000', 'Sofia, ul. Rakovska 21', 0),
+('ORD-2025-0027', 12, '2025-02-27', 'Delivered', '8000', 'Burgas, ul. Strandzha 11', 0),
+('ORD-2025-0028', 13, '2025-02-28', 'Shipped',   '5800', 'Pleven, ul. Skobelev 4', 0),
+('ORD-2025-0029', 14, '2025-03-01', 'Delivered', '9000', 'Varna, ul. Vladislav 9', 0),
+('ORD-2025-0030', 15, '2025-03-02', 'Delivered', '1000', 'Sofia, ul. Dondukov 33', 0);
+
+INSERT INTO [ORDER] (ORDER_NUMBER, [USER_ID], ORDER_DATE, [STATUS], POSTAL_CODE, DELIVERY_ADDRESS, TOTAL_AMOUNT) VALUES
+('ORD-2025-0031', 3,  '2025-03-03', 'Delivered', '9000', 'Varna, ul. Cherno more 12', 0),
+('ORD-2025-0032', 5,  '2025-03-04', 'Delivered', '7000', 'Ruse, ul. Dunav 3',         0),
+('ORD-2025-0033', 8,  '2025-03-05', 'Delivered', '1000', 'Sofia, bul. Bulgaria 15',   0),
+('ORD-2025-0034', 10, '2025-03-06', 'Shipped',   '4000', 'Plovdiv, ul. Kamenitsa 7',  0),
+('ORD-2025-0035', 12, '2025-03-07', 'Delivered', '8000', 'Burgas, ul. Strandzha 11',  0),
+('ORD-2025-0036', 14, '2025-03-08', 'Delivered', '9000', 'Varna, ul. Vladislav 9',    0),
+('ORD-2025-0037', 2,  '2025-03-09', 'Pending',   '4000', 'Plovdiv, ul. Trakia 5',     0),
+('ORD-2025-0038', 4,  '2025-03-10', 'Delivered', '8000', 'Burgas, ul. More 8',        0),
+('ORD-2025-0039', 6,  '2025-03-11', 'Delivered', '6000', 'Stara Zagora, ul. Beroe 22',0),
+('ORD-2025-0040', 9,  '2025-03-12', 'Cancelled', '9000', 'Varna, ul. Sevastopol 1',   0);
+
+
+INSERT INTO ORDER_PRODUCT (ORDER_ID, PRODUCT_ID, QUANTITY, UNIT_PRICE) VALUES
+(1,  52,  1, 1200.00),
+(2,  2,  1, 850.00),
+(3,  3,  1, 950.00),
+(4,  4,  1, 1100.00),
+(5,  5,  1, 600.00),
+(6,  6,  1, 720.00),
+(7,  7,  1, 1300.00),
+(8,  8,  1, 400.00),
+(9,  9,  1, 1450.00),
+(10, 10, 1, 980.00),
+(11, 11, 1, 560.00),
+(12, 12, 1, 770.00),
+(13, 13, 1, 1320.00),
+(14, 14, 1, 890.00),
+(15, 15, 1, 1500.00),
+(16, 16, 1, 640.00),
+(17, 17, 1, 720.00),
+(18, 18, 1, 890.00),
+(19, 19, 1, 300.00),
+(20, 20, 1, 1190.00),
+(21, 21, 1, 450.00),
+(22, 22, 1, 520.00),
+(23, 23, 1, 990.00),
+(24, 24, 1, 760.00),
+(25, 25, 1, 1350.00),
+(26, 26, 1, 620.00),
+(27, 27, 1, 460.00),
+(28, 28, 1, 540.00),
+(29, 29, 1, 880.00),
+(30, 30, 1, 1010.00);
+
+INSERT INTO ORDER_PRODUCT (ORDER_ID, PRODUCT_ID, QUANTITY, UNIT_PRICE) VALUES
+(31, 5,  1, 999.99),
+(31, 48, 1, 79.99),
+(32, 24, 1, 699.99),
+(32, 27, 2, 39.99),
+(33, 12, 1, 349.99),
+(33, 38, 1, 29.99),
+(33, 36, 2, 24.99),
+(34, 21, 1, 199.99),
+(34, 36, 1, 24.99),
+(35, 33, 1, 59.99),
+(35, 27, 1, 14.99),
+(35, 28, 1, 39.99),
+(36, 49, 1, 119.99),
+(36, 40, 1, 49.99),
+(37, 41, 1, 249.99),
+(37, 36, 1, 24.99),
+(37, 27, 1, 34.99),  
+(38, 38, 1, 29.99),
+(38, 30, 1, 69.99),
+(38, 27, 1, 39.99),
+(39, 4,  1, 399.99),
+(39, 52, 1, 119.99),
+(40, 48, 1, 79.99),
+(40, 28, 1, 39.99),
+(40, 36, 1, 24.99);
+
+
+INSERT INTO PAYMENT (ORDER_ID, METHOD, AMOUNT, CURRENCY, STATUS, PAID_AT) VALUES
+(1,  'Card',            0, 'BGN', 'Paid',    '2025-02-01'),
+(2,  'Card',            0, 'BGN', 'Paid',    '2025-02-02'),
+(3,  'Card',            0, 'BGN', 'Paid',    '2025-02-03'),
+(4,  'Cash on delivery',0, 'BGN', 'Paid',    '2025-02-04'),
+(5,  'Card',            0, 'BGN', 'Paid',    '2025-02-05'),
+(6,  'Card',            0, 'BGN', 'Paid',    '2025-02-06'),
+(7,  'Card',            0, 'BGN', 'Paid',    '2025-02-07'),
+(8,  'Cash on delivery',0, 'BGN', 'Paid',    '2025-02-08'),
+(9,  'Card',            0, 'BGN', 'Paid',    '2025-02-09'),
+(10, 'Card',            0, 'BGN', 'Paid',    '2025-02-10'),
+(11, 'Card',            0, 'BGN', 'Paid',    '2025-02-11'),
+(12, 'Card',            0, 'BGN', 'Paid',    '2025-02-12'),
+(13, 'Card',            0, 'BGN', 'Paid',    '2025-02-13'),
+(14, 'PayPal',          0, 'BGN', 'Paid',    '2025-02-14'),
+(15, 'Card',            0, 'BGN', 'Paid',    '2025-02-15'),
+(16, 'Card',            0, 'BGN', 'Paid',    '2025-02-16'),
+(17, 'Card',            0, 'BGN', 'Paid',    '2025-02-17'),
+(18, 'Card',            0, 'BGN', 'Paid',    '2025-02-18'),
+(19, 'Cash on delivery',0, 'BGN', 'Paid',    '2025-02-19'),
+(20, 'Card',            0, 'BGN', 'Paid',    '2025-02-20'),
+(21, 'Card',            0, 'BGN', 'Pending', NULL),
+(22, 'Card',            0, 'BGN', 'Pending', NULL),
+(23, 'Card',            0, 'BGN', 'Pending', NULL),
+(24, 'Card',            0, 'BGN', 'Pending', NULL),
+(25, 'Card',            0, 'BGN', 'Pending', NULL),
+(26, 'Card',            0, 'BGN', 'Paid',    '2025-02-26'),
+(27, 'Card',            0, 'BGN', 'Paid',    '2025-02-27'),
+(28, 'Card',            0, 'BGN', 'Paid',    '2025-02-28'),
+(29, 'PayPal',          0, 'BGN', 'Paid',    '2025-03-01'),
+(30, 'Card',            0, 'BGN', 'Paid',    '2025-03-02');
+
+INSERT INTO PAYMENT (ORDER_ID, METHOD, AMOUNT, CURRENCY, STATUS, PAID_AT) VALUES
+(31, 'Card',            1079.98, 'BGN', 'Paid',     '2025-03-03'),
+(32, 'Card',             779.97, 'BGN', 'Paid',     '2025-03-04'),
+(33, 'Card',             429.96, 'BGN', 'Paid',     '2025-03-05'),
+(34, 'Cash on delivery', 224.98, 'BGN', 'Paid',     '2025-03-06'),
+(35, 'Card',             114.97, 'BGN', 'Paid',     '2025-03-07'),
+(36, 'PayPal',           169.98, 'BGN', 'Paid',     '2025-03-08'),
+(37, 'Card',             309.97, 'BGN', 'Pending',   NULL),         -- още не е платена
+(38, 'Card',             139.97, 'BGN', 'Paid',     '2025-03-10'),
+(39, 'Card',             519.98, 'BGN', 'Paid',     '2025-03-11'),
+(40, 'Card',             144.97, 'BGN', 'Refunded', '2025-03-13');  -- поръчката е Cancelled
+
+
+
+UPDATE p
+SET p.AMOUNT = o.TOTAL_AMOUNT
+FROM PAYMENT p
+JOIN [ORDER] o ON o.ORDER_ID = p.ORDER_ID;
+
+Select * from [ORDER]
